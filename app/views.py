e69c3f506d7057e36193944d7c3996e218e8f3d6
huhaiqng <hu158168>
2020-03-16 17:33:39 +0800

    commit

--- app/views.py
+++ app/views.py
@@ -1,10 +1,27 @@
 # from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.response import Response
 from app.models import Host
 from app.serializers import HostSerializer
+from rest_framework import pagination
 # Create your views here.
 
 
 class HostViewSet(viewsets.ModelViewSet):
     queryset = Host.objects.all()
     serializer_class = HostSerializer
+
+    def list(self, request, *args, **kwargs):
+        pagination.PageNumberPagination.page_size = request.GET.get('limit')
+        ip = request.GET.get('ip')
+        type = request.GET.get('type')
+        env = request.GET.get('env')
+        queryset = Host.objects.filter(ip__contains=ip,type__contains=type,env__contains=env).order_by('ip')
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
