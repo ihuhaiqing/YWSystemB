90ba701cae1c239bdb561e1cb58ef604ca44aa32
huhaiqng <hu158168>
2020-03-30 15:07:54 +0800

    commit

--- YWSystemB/settings.py
+++ YWSystemB/settings.py
@@ -74,16 +74,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'YWSystemB.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
