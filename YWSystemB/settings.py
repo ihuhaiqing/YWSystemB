54994826826b7220fe1171fd30e749d99f53e1b9
huhaiqing <lixin@gainhon.com>
2020-08-08 15:08:02 +0800

    commit

--- YWSystemB/settings.py
+++ YWSystemB/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 OAUTH2_PROVIDER = {
     # this is the list of available scopes
