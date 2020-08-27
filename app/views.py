bc91a36f8c73c2df4c756d1aa938930e2a5201e1
huhaiqing <lixin@gainhon.com>
2020-08-27 17:33:14 +0800

    commit

--- app/views.py
+++ app/views.py
@@ -227,6 +227,26 @@ class ProjectViewSet(CheckPermViewSet):
 class GetProjectViewSet(CheckPermViewSet):
     queryset = Project.objects.all()
     serializer_class = GetProjectSerializer
+    pagination_class = PageNumberPagination
+
+    def list(self, request, *args, **kwargs):
+        page_size = request.GET.get('limit')
+        if int(page_size) == 10000:
+            PageNumberPagination.page_size = None
+        else:
+            PageNumberPagination.page_size = page_size
+        objects = Project.objects.all()
+        queryset = get_objects_for_user(request.user, 'app.view_%s' % self.basename, objects)
+        page = self.paginate_queryset(queryset)
+
+        # 将 PageNumberPagination.page_size 设置为 None 以免影响其它查询
+        PageNumberPagination.page_size = None
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
 
 
 # Project Web
