89125507e82aed1d3571e743c1e990a82ad60775
huhaiqing <lixin@gainhon.com>
2020-11-25 11:15:35 +0800

    commit

--- app/models.py
+++ app/models.py
@@ -5,8 +5,8 @@ from django.utils import timezone
 
 # 账号
 class Account(models.Model):
-    use = models.CharField('名称', max_length=255)
-    username = models.CharField('用户名', max_length=200, unique=True)
+    use = models.CharField('名称', max_length=255, unique=True)
+    username = models.CharField('用户名', max_length=200)
     password = models.CharField('密码', max_length=200)
     addr = models.CharField('地址', max_length=200, blank=True, null=True)
     remark = models.CharField('备注', max_length=255, blank=True)
