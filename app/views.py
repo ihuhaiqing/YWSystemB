ec2a66287b52c5bbb6a6d90cffe61670e0da55e3
huhaiqing <lixin@gainhon.com>
2020-12-16 13:55:56 +0800

    commit

--- app/views.py
+++ app/views.py
@@ -49,8 +49,9 @@ class GetDashboardDataView(APIView):
     def get(self, request):
         project_count = Project.objects.count()
         host_count = Host.objects.count()
+        account_count = Account.objects.count()
 
-        return Response({'project_count':project_count,'host_count':host_count})
+        return Response({'project_count': project_count, 'host_count': host_count, 'account_count': account_count})
 
 
 # 主机
