0af08e8ca4ef08cb4c196a73eb4ba4a036a92212
huhaiqing <lixin@gainhon.com>
2021-04-13 16:24:02 +0800

    commit

--- app/models.py
+++ app/models.py
@@ -385,9 +385,9 @@ class ProjectCodeaddr(models.Model):
 
 # ---------------------------------------------------------------------------------------
 class Task(models.Model):
-    name = models.CharField('名称',max_length=200,unique=True)
+    name = models.CharField('名称', max_length=200, unique=True)
     script_dir = models.CharField('脚本路径', max_length=200)
-    script_name = models.CharField('脚本名',max_length=200,unique=True)
-    arg = models.CharField('参数',max_length=200,blank=True)
-    type = models.CharField('类别',max_length=200)
+    script_name = models.CharField('脚本名', max_length=200, unique=True)
+    arg = models.CharField('参数', max_length=200, blank=True)
+    type = models.CharField('类别', max_length=200)
     created = models.DateTimeField('创建时间', default=timezone.now)
